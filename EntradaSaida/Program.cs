141694bb355ac046172bcf1b5d84de00102805a0
Luanaap <57630311+Luanaap@users.noreply.github.com>
2023-02-24 23:24:08 -0300

    Criando repositorio para Entrada e Saida

---
+++ EntradaSaida/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Luana ");
+            Console.WriteLine("Artigas");
+
+            //int letra = Console.Read();
+
+            string texto = Console.ReadLine();
+
+            Console.WriteLine(texto);
+
+            //Console.ReadKey(true);
+            Console.ReadKey();
+        }
+    }
+}
